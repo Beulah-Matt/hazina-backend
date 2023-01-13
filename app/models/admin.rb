787b432fdea52f5e81637f2b248c8f15571aa255
Beulah Mathenge <beulahmathenge@gmail.com>
2023-01-13 23:37:40 +0300

    fixed typo

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,6 @@ class Admin < ApplicationRecord
     has_secure_password
     has_many :storage_units
 
-    validates :name, presence: :true,
-    validates :email, presence: :true, uniqueness: :true
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
